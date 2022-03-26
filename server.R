20293d85a56036ad37510f09f11ca5ac1f22acbc
jolie <jolievfx@gmail.com>
2022-03-25 19:01:59 -0700

    initialise app

--- server.R
+++ server.R
@@ -1,33 +1,44 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
-# example from https://shiny.rstudio.com/gallery/kmeans-example.html
-
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-
-# Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output, session) {
-
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+   
+    #Use those radioButtons to select the data to use
+    #Note that the input was called "dataSource" in the ui.R file
+    #Here it is an element from a list.
+    #I extract it using input$nameOfThing
+    if (input$dataSource == "DataFile2006") {
+      x = read.csv("pop2006.csv")
+    } else {
+      if(input$dataSource == "DataFile2011"){
+        x = read.csv("pop2011.csv")
+      }else{
+        #This means do not use any data.  This will break things.
+        x=NULL
+      }
+    }
+    #I didn't need to use if{}else{if{}else{}}
+    # but I do so to show how to use multiple if else statements.
+    
+    
+    # generate bins based on input$bins from ui.R
+    x    <- x[-1, 3] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
   })
-
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
-
-autoInvalidate <- reactiveTimer(10000)
-  observe({
-    autoInvalidate()
-    cat(".")
-  })
-
-  output$plot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
-
+  
 })
